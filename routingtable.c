498a70f4895d706521e32f8c6964bb483a685c3c
Terrence J Randall Jr <randall7@ecegrid-thin1.ecn.purdue.edu>
2019-10-27 14:58:32 -0400

    progress baby

--- routingtable.c
+++ routingtable.c
@@ -9,21 +9,52 @@ int NumRoutes;
 
 ////////////////////////////////////////////////////////////////
 void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID){
-	/* ----- YOUR CODE HERE ----- */
+  //Initializing the routing table with thte info from InitRespons
+  //myID is recieved from the command line
+
+  //ASSUMPTION: That I'll load the single router's table into this single data structure
+  //and that the other routers will be running on different ports
+  int i;
+  numRoutes = 0;
+  for(i = 0; i< InitResponse->no_nbr; i++) {
+    routingTable[i].dest_id = InitResponse->nbrcost[i].nbr;
+    routingTable[i].cost = InitResponse->nbrcost[i].cost;
+    routingTable[i].next_hop = InitResponse->nbrcost[i].nbr; //Might not need this
+    numRoutes++;
+  }
+  routingTable[i].dest_id = myID;
+  routingTable[i].cost = 0;
+  routingTable[i].next_hop = myID; //Might not need this either
+
+  numRoutes++;  //Not sure if the router pointing back to itself is a route
 	return;
 }
 
 
 ////////////////////////////////////////////////////////////////
 int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myID){
-	/* ----- YOUR CODE HERE ----- */
+  int i;
+  int q;
+  //Comparing the id's to see if theres a shorter distance
+  for(i = 0; i<RecvdUpdatePacket->no_routes, i++) {
+    for(q = 0; q <no_routes; q++) {
+
+    }
+  }
+  
 	return 0;
 }
 
 
 ////////////////////////////////////////////////////////////////
 void ConvertTabletoPkt(struct pkt_RT_UPDATE *UpdatePacketToSend, int myID){
-	/* ----- YOUR CODE HERE ----- */
+  UpdatePacketToSend->sender_id = myID;
+  //POTENTIAL ERROR
+  //How wil I send this to every neighbor (potential threading oppurtunity)
+  UpdatePacketToSend->dest_id = routingTable[0].dest_id;
+
+  UpdatePacketToSend->no_routes = numRoutes;
+  UpdatePacketToSend->route = routingTable;
 	return;
 }
 
